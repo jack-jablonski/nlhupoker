a55a5a86f71a912d79b78338d8d4c6dc284380cd
Alexander Nyberg <alenyb@gmail.com>
2013-06-23 20:21:55 +0200

    Clearer comments of EquityTable

--- src/main/java/se/hupoker/cards/handeval/EquityTable.java
+++ src/main/java/se/hupoker/cards/handeval/EquityTable.java
@@ -6,14 +6,14 @@ import java.io.Serializable;
 import java.util.Arrays;
 
 /**
+ * Equities when all cards are run out on a given board.
+ *
  * @author Alexander Nyberg
  */
 class EquityTable implements Serializable {
     /*
-     *  Equities when all cards run out
      *  TODO: Half size from get(i,j) = 1 - get(j,i)
-     *  TODO: Should possibly be a Table<HoleCards, HoleCards, Float> but would be gigantic?
-     *
+     *  TODO: Should ideally be a Table<HoleCards, HoleCards, Float> but would be gigantic?
      */
     private final float[][] equities = new float[HoleCards.TexasCombinations][HoleCards.TexasCombinations];
 
